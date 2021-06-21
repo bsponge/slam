c0454d1fc652f7fe59184666b415ce9612479218
bsponge <bsponge@github.com>
2021-06-21 23:41:21 +0200

    add fundamental and essential matrix

--- Matcher.py
+++ Matcher.py
@@ -4,9 +4,6 @@ class Matcher:
 
     def match(self, kp1, kp2, des1, des2):
         matches_all = self.matcher.knnMatch(des1, des2, k=2)
-        #matches_all = matcher.match(des1, des2)
-        #matches_gms = matchGMS(shape1[:2], shape2[:2], kp1, kp2, matches_all)
-        #matches_gms = sorted(matches_gms, key = lambda x: x.distance)
         good = []
         for m,n in matches_all:
             # 0.50 ???
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import cv2 as cv
 import numpy as np
 from cv2.xfeatures2d import matchGMS
+from skimage import data
+from skimage.color import rgb2gray
+from skimage.feature import match_descriptors, ORB, plot_matches
+from skimage.measure import ransac
+from skimage.transform import FundamentalMatrixTransform
 import numpy as np
 import sys
 import kdtree
@@ -83,35 +88,45 @@ def main():
     while cap.isOpened(): 
         # get frame
         ret, frame = cap.read()
-        frame = cv.resize(frame, (frame.shape[1]//2, frame.shape[0]//2))
+        if frame is not None:
+            frame = cv.resize(frame, (frame.shape[1]//2, frame.shape[0]//2))
         kp1, kp2, matches = None, None, None
 
-        frames = []
-        # analyze first 6 frames to locate points in space with kdtree
-        """
-        for i in range(6):
-            frames.append(cap.read()[1])
-
-        for i in range(len(frames)-1):
-            prev = frames[i]
-            now = frames[i+1]
-            kp1, des1 = orb.detectAndCompute(prev, None)
-            kp2, des2 = orb.detectAndCompute(now, None)
-            matches = match(kp1, kp2, des1, des2, prev.shape, now.shape, orb, matcher)
-            
-        """
-
-        if last_frame is not None and frame is not None:
-            
-            # get keypoints and descriptors
+        if last_frame is not None and frame is not None: 
+            points_num = 0
             kp1, des1 = orb.detectAndCompute(last_frame, None)
             kp2, des2 = orb.detectAndCompute(frame, None)
-            # get matches
+
             matches = matcher.match(kp1, kp2, des1, des2)
-            # iterate through matches and save matching points to the file
-            points_num = 0
+            pts1 = []
+            pts2 = []
+
             for m in matches:
-                if m.distance < 80:
+                if m.distance < 30:
+                    pts1.append(kp1[m.queryIdx].pt)
+                    pts2.append(kp2[m.trainIdx].pt)
+
+            pts1 = np.asarray(pts1)
+            pts2 = np.asarray(pts2)
+            print(f'pts1: {pts1.shape}, pts2: {pts2.shape}')
+
+            print(f'lastframeshape: {last_frame.shape}, frameshape: {frame.shape}')
+
+            if pts1.shape[0] != 0 and pts2.shape[0] != 0:
+                F, mask = cv.findFundamentalMat(pts1, pts2, cv.FM_8POINT)
+                inv = np.linalg.inv(intrinsic_matrix)
+                E = inv.transpose().dot(F).dot(intrinsic_matrix)
+                print('fundamental')
+                print(F)
+                print('essential')
+                print(E)
+                u, s, vh = np.linalg.svd(E)
+
+
+            """
+            for m in matches:
+                #if m.distance < 80:
+                if True:
                     # y = mx + b
                     div1 = center[0]-kp1[m.queryIdx].pt[0]
                     if div1 != 0.0:
@@ -126,6 +141,7 @@ def main():
 
                     if dx != 0.0:
                         slope = (kp1[m.queryIdx].pt[1]-kp2[m.trainIdx].pt[1]) / dx
+
 
                     # ? probably doesn't matter
                     if slope < 14 and slope > -14:
@@ -155,14 +171,41 @@ def main():
                         file.write(str(pt[2][0] + position[2]))
                         file.write('\n')
                         points_num = points_num + 1
+            """
+            
+            """
+            last_frame, frame = map(rgb2gray, (last_frame, frame))
+            descriptor_extractor = ORB()
+            print(last_frame.shape)
+            descriptor_extractor.detect_and_extract(last_frame)
+            keypoints_left = descriptor_extractor.keypoints
+            descriptors_left = descriptor_extractor.descriptors
+
+            descriptor_extractor.detect_and_extract(frame)
+            keypoints_right = descriptor_extractor.keypoints
+            descriptors_right = descriptor_extractor.descriptors
+            matches = match_descriptors(descriptors_left, descriptors_right,
+                            cross_check=True)
+
+            model, inliers = ransac((keypoints_left[matches[:, 0]],
+                                     keypoints_right[matches[:, 1]]),
+                                     FundamentalMatrixTransform, min_samples=8,
+                                     residual_threshold=1, max_trials=5000)
+
+            inlier_keypoints_left = keypoints_left[matches[inliers, 0]]
+            inlier_keypoints_right = keypoints_right[matches[inliers, 1]]
+
+            print(f"Number of matches: {matches.shape[0]}")
+            print(f"Number of inliers: {inliers.sum()}")
+            """
             points_in_frame.write(str(points_num))
             points_in_frame.write('\n')
             position = position[0], position[1], position[2] + 5
         # display frames of the video
+        last_frame = frame
         if ret:
-            if frame is not None:
-                center = (frame.shape[1]//2, frame.shape[0])
-            gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)  
+
+            """
             kp = orb.detect(frame, None)
             if frame is not None and kp2 is not None:
                 img = cv.drawKeypoints(frame, [kp2[m.trainIdx] for m in matches], frame)
@@ -170,15 +213,14 @@ def main():
             if img is not None and matches is not None:
                 for m in matches[100:110]:
                     cv.line(img, (int(kp1[m.queryIdx].pt[0]), int(kp1[m.queryIdx].pt[1])), (int(kp2[m.trainIdx].pt[0]), int(kp2[m.trainIdx].pt[1])), (255,0,0), 1)
-                    """
                     d = kp1[m.queryIdx].pt[0] - kp2[m.trainIdx].pt[0]
                     if d != 0.0:
                         img = cv.putText(img, str((kp1[m.trainIdx].pt[1]-kp2[m.trainIdx].pt[1])/d), (int(kp1[m.queryIdx].pt[0]), int(kp1[m.queryIdx].pt[1])), cv.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 1, cv.LINE_AA)
-                    """
             # perspective lines
             cv.imshow('Frame',img)
             #sys.stdin.readline()
             last_frame = frame
+            """
             if cv.waitKey(25) & 0xFF == ord('q'):
                 break
         else:
