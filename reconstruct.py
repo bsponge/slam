216c4605664b2e5312b8188225b71488df195b39
Jakub Skiba <kubask03@gmail.com>
2022-10-29 23:37:30 +0200

    cleanup

--- reconstruct.py
+++ reconstruct.py
@@ -15,26 +15,6 @@ import math
 from Analyser import *
 from Matcher import *
 
-def triangulate(pose1, pose2, pts1, pts2):
-    ret = np.zeros((pts1.shape[0], 4))
-
-    for i, p in enumerate(zip(pts1, pts2)):
-        A = np.zeros((4,4))
-        A[0] = p[0][0] * pose1[2] - pose1[0]
-        A[1] = p[0][1] * pose1[2] - pose1[1]
-        A[2] = p[1][0] * pose2[2] - pose2[0]
-        A[3] = p[1][1] * pose2[2] - pose2[1]
-        _, _, vt = np.linalg.svd(A)
-        ret[i] = vt[3]
-    
-    return ret
-
-"""
-class Reconstruct:
-    def __init__(self, first_frame, second_frame):
-        
-
-"""
 
 file = open('points', 'w')
 points_in_frame = open('points_in_frame', 'w')
