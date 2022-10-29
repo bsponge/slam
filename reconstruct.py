9b8b63219e9c508d6fbb604a22db5572a09a1092
Jakub Skiba <kubask03@gmail.com>
2022-10-29 23:36:33 +0200

    maybe something dunno

--- reconstruct.py
+++ reconstruct.py
@@ -15,6 +15,20 @@ import math
 from Analyser import *
 from Matcher import *
 
+def triangulate(pose1, pose2, pts1, pts2):
+    ret = np.zeros((pts1.shape[0], 4))
+
+    for i, p in enumerate(zip(pts1, pts2)):
+        A = np.zeros((4,4))
+        A[0] = p[0][0] * pose1[2] - pose1[0]
+        A[1] = p[0][1] * pose1[2] - pose1[1]
+        A[2] = p[1][0] * pose2[2] - pose2[0]
+        A[3] = p[1][1] * pose2[2] - pose2[1]
+        _, _, vt = np.linalg.svd(A)
+        ret[i] = vt[3]
+    
+    return ret
+
 """
 class Reconstruct:
     def __init__(self, first_frame, second_frame):
@@ -27,49 +41,27 @@ points_in_frame = open('points_in_frame', 'w')
 camera_pose_file = open('camera_pose', 'w')
 camera_points_file = open('camera_points', 'w')
 
-first_img = cv2.imread('remote1.jpg')
-second_img = cv2.imread('remote2.jpg')
+first_img = cv2.imread('1.jpg')
+second_img = cv2.imread('2.jpg')
 third_img = cv2.imread('remote3.jpg')
 
 height, width = first_img.shape[:2]
-print((height,width))
+print("height and width: ", (height,width))
 
 intrinsic_matrix = np.array([
-    [5500,   0,    width/2 ],
-    [0,   5500,    height/2],
+    [600,   0,    width/2 ],
+    [0,   600,    height/2],
     [0,     0,    1       ]
 ])
 
-# find better way to extract matching features and match them
-'''
 matcher = Matcher()
 pts1, pts2 = matcher.match(first_img, second_img)
-'''
-orb = cv2.ORB_create(10000)
-mth = cv2.BFMatcher(cv2.NORM_HAMMING)
 
-kp1, des1 = orb.detectAndCompute(first_img, None)
-kp2, des2 = orb.detectAndCompute(second_img, None)
-
-matches = mth.knnMatch(des1, des2, k=2)
-
-pts1 = []
-pts2 = []
-
-for m, n in matches:
-    if m.distance < 0.8*n.distance:
-        pts1.append(kp1[m.queryIdx].pt)
-        pts2.append(kp2[m.trainIdx].pt)
-
-pts1 = np.asarray(pts1)
-pts2 = np.asarray(pts2)
-
-E, _ = cv2.findEssentialMat(pts1, pts2, intrinsic_matrix) # last 3 args , cv2.RANSAC, 0.99, 0.01
+E, _ = cv2.findEssentialMat(pts1, pts2, intrinsic_matrix, cv2.RANSAC, 0.99, 0.01) # last 3 args , cv2.RANSAC, 0.99, 0.01
 _, R, t, _ = cv.recoverPose(E, pts1, pts2, intrinsic_matrix)
 
-print(np.zeros((3,1)))
 first_camera_matrix = np.hstack((intrinsic_matrix, np.zeros((3,1))))
-second_camera_matrix = np.hstack((R, t.reshape((3, 1))))
+second_camera_matrix = intrinsic_matrix.dot(np.hstack((R, t.reshape((3, 1)))))
 
 print('first camera matrix')
 print(first_camera_matrix)
@@ -84,16 +76,17 @@ print(pts2.shape)
 pts1 = pts1.T
 pts2 = pts2.T
 
-scale = 1.0
+scale = 0.05
 
 triangulated_points = cv2.triangulatePoints(first_camera_matrix, second_camera_matrix, pts1, pts2).T
+#triangulated_points = triangulate(first_camera_matrix, second_camera_matrix, pts1, pts2)
 
 for i in range(triangulated_points.shape[0]):
-    file.write(str(scale*triangulated_points[i, 0])) # 
+    file.write(str(scale*triangulated_points[i, 0] / triangulated_points[i,3])) # 
     file.write(' ')
-    file.write(str(scale*triangulated_points[i, 1])) # 
+    file.write(str(scale*triangulated_points[i, 1] / triangulated_points[i,3])) # 
     file.write(' ')
-    file.write(str(scale*triangulated_points[i, 2])) # 
+    file.write(str(scale*triangulated_points[i, 2] / triangulated_points[i,3])) # 
     file.write('\n')
 points_num = triangulated_points.shape[0]
 points_in_frame.write(str(points_num))
